677b647d527c145b8c4737c0d19fc12662e59ffd
Kirllik <robotsmirnoff@yandex.ru>
2025-09-25 22:29:09 +0300

    Init-d3

--- src/app/ui/app.jsx
+++ src/app/ui/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { LiquidityCheckPage } from '../../pages';
-import styles from './app.module.scss'; //
+import styles from './app.module.scss'; //сс
 
 export const App = () => {
   return (
